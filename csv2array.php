8330ee45fa7eae0ff2e4fc36bc51d2b66388882c
ditasmer <ditasmer@gmail.com>
2020-08-03 11:15:59 +0200

    Convertir fila en array alumno

--- csv2array.php
+++ csv2array.php
@@ -10,5 +10,28 @@ eregi_replace("[\n|\r|\n\r|\t]","" , $texto)
 ●Este array lo guardamos en un segundo array que tendrá tantas filas como registros tenga el fichero:
 array_push($registros, $fila)*/
 
+//convertir csv a array
+
+//proteger lectura
+if(file_exists('309_alumnos.csv')){
+	//abrir archivo modo lectura
+	$fichero = fopen('309_alumnos.csv', 'r');
+
+	//primera lectura, descartar primera fila
+	fgets($fichero);
+
+	//leer hasta el final
+	while(!feof($fichero)){
+	//lectura linea a linea
+		$fila = fgets($fichero);
+
+		//guarda fila en array 
+		$alumno = explode(';', $fila);
+		print_r($alumno);
+		echo '<br>';
+	}
+}
+
+	
 
 ?>
